04043bcb27ffb18c72481c46f863bbafca365d0e
Lee I-Shiang <ishianglee@gmail.com>
2022-04-01 22:59:03 +0800

    Add classification models

--- app.py
+++ app.py
@@ -28,6 +28,18 @@ json_file.close()
 bert_keras_model = model_from_json(bertmodel_json)
 bert_keras_model.load_weights("bertmodel.h5")
 
+json_file = open("spectermodel_classification.json", "r")
+spectermodel_classification_json = json_file.read()
+json_file.close()
+specter_classification_keras_model = model_from_json(spectermodel_classification_json)
+specter_classification_keras_model.load_weights("spectermodel_classification.h5")
+
+json_file = open("bertmodel_classification.json", "r")
+bertmodel_classification_json = json_file.read()
+json_file.close()
+bert_classification_keras_model = model_from_json(bertmodel_classification_json)
+bert_classification_keras_model.load_weights("bertmodel_classification.h5")
+
 print('Go to http://127.0.0.1:5000/')
 
 
@@ -36,10 +48,12 @@ def response():
     model_name = request.form['action']
     text = request.form.get("text")
 
-    if model_name == 'both!':
+    if model_name == 'compare all!':
         sentences = []
         specter_keywords = []
         bert_keywords = []
+        specter_classification_keywords = []
+        bert_classification_keywords = []
 
         for sentence in text.split('.'):
             if sentence == '':
@@ -74,17 +88,35 @@ def response():
             bert_keywords += [mod_sentence.split(" ")[
                                   round(bert_keras_model.predict(np.array([bert_encoded]))[0][0])
                               ]]
+            specter_classification_keywords += [mod_sentence.split(" ")[
+                                                    min(np.argmax(specter_classification_keras_model.predict(
+                                                        np.array([specter_encoded]))[0]),
+                                                        len(mod_sentence.split(" ")) - 1,
+                                                        )
+                                                ]]
+            bert_classification_keywords += [mod_sentence.split(" ")[
+                                                 min(np.argmax(
+                                                     bert_classification_keras_model.predict(np.array([bert_encoded]))[
+                                                         0]),
+                                                     len(mod_sentence.split(" ")) - 1,
+                                                 )
+                                             ]]
 
         merged = ""
-        for i in zip(sentences, specter_keywords, bert_keywords):
-            merged += "sentence: " + i[0] + "\nspecter keyword: " + i[1] + "\nbert keyword: " + i[2] + "\n\n\n"
+        for i in zip(sentences, specter_keywords, bert_keywords, specter_classification_keywords,
+                     bert_classification_keywords):
+            merged += "sentence: " + i[0] + "\nspecter keyword: " + i[1] + "\nbert keyword: " + i[
+                2] + "\nspecter (classification) keyword: " + i[3] + "\nbert (classification) keyword: " + i[
+                          4] + "\n\n\n"
     else:
-        if model_name == 'allenai-specter':
+        is_classification = 'classification' in model_name
+
+        if 'allenai-specter' in model_name:
             model = specter_model
-            keras_model = specter_keras_model
-        elif model_name == 'bert-base-nli-mean-tokens':
+            keras_model = specter_classification_keras_model if is_classification else specter_keras_model
+        elif 'bert-base-nli-mean-tokens' in model_name:
             model = bert_model
-            keras_model = bert_keras_model
+            keras_model = bert_classification_keras_model if is_classification else bert_keras_model
 
         sentences = []
         keywords = []
@@ -115,8 +147,11 @@ def response():
 
             sentences += [sentence]
             keywords += [mod_sentence.split(" ")[
-                             round(keras_model.predict(np.array([encoded]))[0][0])
-                         ]]
+                             min(np.argmax(keras_model.predict(np.array([encoded]))[0]),
+                                 len(mod_sentence.split(" ")) - 1,
+                                 )
+                         ]] if is_classification else [
+                mod_sentence.split(" ")[round(keras_model.predict(np.array([encoded]))[0][0])]]
 
         merged = ""
         for i in zip(sentences, keywords):
